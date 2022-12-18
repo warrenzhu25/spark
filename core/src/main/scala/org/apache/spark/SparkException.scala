097dc928e625684c2b3157681abf780e3c67a7f6
Warren Zhu <warren.zhu25@gmail.com>
2022-12-17 18:26:04 -0800

    [SPARK-41954][CORE] Add isDecommissioned in ExecutorDeadException
    Change IsExecutorAlive to return tuple (isExecutorAlive, isDecommissioned), then throw ExecutorDeadException with isisDecommissioned
    
    b/269306187
    
    Change-Id: I8a49aa4eaa277ff7cfa310e3a6320bacb074b10d
    Reviewed-on: https://bigdataoss-internal-review.googlesource.com/c/third_party/apache/spark/+/53520
    Reviewed-by: Igor Dvorzhak <idv@google.com>
    Reviewed-by: Animesh Nandanwar <animeshvn@google.com>
    Reviewed-by: Wei Yan <weiyans@google.com>
    Tested-by: Prow Service Account <425329972751-compute@developer.gserviceaccount.com>

--- core/src/main/scala/org/apache/spark/SparkException.scala
+++ core/src/main/scala/org/apache/spark/SparkException.scala
@@ -65,7 +65,7 @@ private[spark] case class SparkUserAppException(exitCode: Int)
 /**
  * Exception thrown when the relative executor to access is dead.
  */
-private[spark] case class ExecutorDeadException(message: String)
+private[spark] case class ExecutorDeadException(isDecommissioned: Boolean, message: String)
   extends SparkException(message)
 
 /**
